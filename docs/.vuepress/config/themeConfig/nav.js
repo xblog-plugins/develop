86bc7f5bfe9cb363670d9db8765124420119151f
xiaoyou66 <xiaoyou2333@foxmail.com>
2021-08-01 17:13:03 +0800

    add: 大致完成大部分内容

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -5,13 +5,14 @@ module.exports = [
     link: '/plugins/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       { text: '快速入门', link: '/pages/175875/' },
+      { text: 'API参考', link: '/pages/8f62f7/' },
     ],
   },  {
     text: '主题开发',
     link: '/themes/',
-  },
-  {
-    text: 'APP下载',
-    link: 'https://www.yd-mobile.cn/pack/download?versionId=2497&packName=com.xiaoyou.interview'
+    items: [
+      { text: '快速入门', link: '/pages/057989/' },
+      { text: 'API参考', link: '/pages/c09383/' }
+    ],
   }
 ]
